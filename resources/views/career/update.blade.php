9a3f6b587dd2374b6ee96ae954d35388a8b5810f
shahbazeng <shahbazeng519@gmail.com>
2022-12-22 01:12:32 +0500

    validations change

--- resources/views/career/update.blade.php
+++ resources/views/career/update.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container"style="padding-top: 4.5rem!important;">
     <div class="row justify-content-center">
         <div class="col-md-9">
             <div class="card">
