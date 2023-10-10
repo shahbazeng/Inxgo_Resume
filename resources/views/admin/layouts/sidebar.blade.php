f357eaa223c946107c06f12969c9a2c7a2cbf8e5
Zain Ul Rauf <bilalkhalid19911@gmail.com>
2023-10-10 21:00:12 +0500

    changes

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
     <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
     <a class="navbar-brand m-0" href="{{ url('admin') }}" target="_blank">
       <img src="{{url('assets/img/logo-ct.png')}}" class="navbar-brand-img h-100" alt="main_logo">
-      <span class="ms-1 font-weight-bold text-white">Material Dashboard 2</span>
+      <span class="ms-1 font-weight-bold text-white">Resume Builder Pannel</span>
     </a>
   </div>
   <hr class="horizontal light mt-0 mb-2">
